9e8069c15a078a3ee0699053d011a17708dc221b
M Braga <mateus.braga@rebase.com.br>
2020-01-31 16:12:32 -0300

    primeiro exercicio

---
+++ src/__tests__/exercicios/Primeiro.test.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { render } from "@testing-library/react";
+
+describe("Lista com items", () => {
+  it("renderiza uma <ul />", () => {});
+  it("deve renderizar 5 <li />", () => {});
+  it("as tags <li /> devem conter um conteudo especifico", () => {});
+});
