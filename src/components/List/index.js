69e297e6cef931adf184e6eafceaa250fe8a62f8
Mateus Braga <mateuscbraga@gmail.com>
2020-02-01 17:04:49 -0300

    solucao 2 exercicio

---
+++ src/components/List/index.js
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+
+function List({ items }) {
+  const [listItems, setListItem] = useState(items);
+  const [item, setItem] = useState("");
+
+  const addItem = () => {
+    setListItem([...listItems, { key: Math.random(4), name: item }]);
+    setItem("");
+  };
+
+  return (
+    <>
+      <ul>
+        {listItems &&
+          listItems.map(item => <li key={item.key}>{item.name}</li>)}
+      </ul>
+      <input type="text" value={item} onChange={e => setItem(e.target.value)} />
+      <button data-testid="add-item" onClick={addItem}>
+        Adicionar item
+      </button>
+    </>
+  );
+}
+
+export default List;
