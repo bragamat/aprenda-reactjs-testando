aec207a9380c8039904aa8c0f440715dc45b93c7
Mateus Braga <mateuscbraga@gmail.com>
2020-02-01 15:36:02 -0300

    primeiro exercicio escrito

--- src/__tests__/exercicios/1.test.js
+++ src/__tests__/exercicios/1.test.js
@@ -1,14 +1,29 @@
 import React from "react";
 import { render } from "@testing-library/react";
 
+import List from "../../components/List";
+
 describe("Lista com items", () => {
   it("renderiza uma <ul />", () => {
-    console.log("Escrever o teste");
+    const { container, getByRole } = render(<List />);
+
+    expect(container.querySelector("ul")).toBeInTheDocument();
+    expect(getByRole("list")).toBeTruthy();
   });
-  it("deve renderizar 5 <li />", () => {
-    console.log("Escrever o teste");
-  });
-  it("as tags <li /> devem conter um conteudo especifico", () => {
-    console.log("Escrever o teste");
+
+  it("deve renderizar 5 <li /> com conteudo especifico", () => {
+    const listaItens = [
+      { key: Math.random(4), name: "Item um" },
+      { key: Math.random(4), name: "Item dois" },
+      { key: Math.random(4), name: "Item tres" },
+      { key: Math.random(4), name: "Item quatro" },
+      { key: Math.random(4), name: "Item cinco" }
+    ];
+    const { container, getAllByText, queryByText } = render(
+      <List items={listaItens} />
+    );
+    expect(container.querySelectorAll("li").length).toBe(listaItens.length);
+    expect(getAllByText(/Item/).length).toBe(listaItens.length);
+    expect(queryByText("Item seis")).not.toBeInTheDocument();
   });
 });
