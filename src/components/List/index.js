ad8fed6fbbe7bac3a94eb786acf5f5e4a52275c9
Mateus Braga <mateuscbraga@gmail.com>
2020-02-02 16:42:29 -0300

    wip: solucao/1

--- src/components/List/index.js
+++ src/components/List/index.js
@@ -1,9 +1,26 @@
 import React from "react";
 
+<<<<<<< Updated upstream
 function List({ items }) {
   return (
     <ul>{items && items.map(item => <li key={item.key}>{item.name}</li>)}</ul>
+=======
+function List() {
+  return (
+    <ul>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+    </ul>
+>>>>>>> Stashed changes
   );
 }
 
 export default List;
+<<<<<<< Updated upstream
+=======
+
+//{items && items.map(item => <li key={item.key}>{item.name}</li>)}
+>>>>>>> Stashed changes
